bc1423401de6a1fb9b61be29f8e9eda8b6a90892
EasonChen <yschen1124@gmail.com>
2022-12-22 20:54:56 +0800

    [Lab08b][create use file]

---
+++ HeapSort.c
@@ -0,0 +1,12 @@
+#include "heap.h"
+//use heap sort
+/***from select sort, we need through all the value which in the list
+ * to pick up the smallest value and its index.
+ * So we use heap to find the biggest value. And put it to the end of list.
+ * then, bottom up the heap inorder to find the second bigger value.
+ * again and again, then we will get the sorting list.
+ ***/
+ int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
