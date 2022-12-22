4087454c85e43d10c77d9ca5f337c563a9b4c589
EasonChen <yschen1124@gmail.com>
2022-12-22 21:48:59 +0800

    [Lab08b][heap sort]

--- HeapSort.c
+++ HeapSort.c
@@ -1,5 +1,7 @@
 #include "heap.h"
 //use heap sort
+void HeapSort(int * heap, int size);
+
 /***from select sort, we need through all the value which in the list
  * to pick up the smallest value and its index.
  * So we use heap to find the biggest value. And put it to the end of list.
@@ -7,6 +9,22 @@
  * again and again, then we will get the sorting list.
  ***/
  int main() {
-    printf("Hello, World!\n");
+    int *data=RandomData(),size= SIZE;
+    printf("original list:\n");PrintList(data,size);
+    int *heap=Heapify(data,size);
+    printf("heapify:\n");PrintList(heap,size);
+    HeapSort(heap,size);
+    printf("Content of array:\n");PrintList(heap,size);
     return 0;
+}
+
+void HeapSort(int * heap, int size) {
+    int unSortSize=size;
+    while (unSortSize>0){
+        Swap(&heap[--unSortSize],&heap[0]);
+        TopDown(heap,unSortSize);
+        if(unSortSize==0)break;
+        printf("Reheaping with the smaller array ...\n");
+        PrintList(heap,unSortSize);
+    }
 }
--- heap.h
+++ heap.h
@@ -1,2 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "generalfunction.h"
+
+void BottomUp(int list[],int size){//(i-1)/2
+    int newValueIndex=size-1;
+    while(newValueIndex>0 && list[newValueIndex]>list[(newValueIndex-1)/2]) {
+        Swap(&list[newValueIndex], &list[(newValueIndex - 1) / 2]);
+        newValueIndex=(newValueIndex - 1) / 2;
+    }
+}
+
+void TopDown(int list[],int size){//i*2+1 i*2+2
+    int newValueIndex=0;
+    while(newValueIndex*2+1<size){
+        int maxChildIndex;
+        if(newValueIndex*2+2<size) {
+            if (list[newValueIndex * 2 + 1] > list[newValueIndex * 2 + 2])maxChildIndex = newValueIndex * 2 + 1;
+            else maxChildIndex = newValueIndex * 2 + 2;
+        }else maxChildIndex= newValueIndex * 2 + 1;
+        if(list[newValueIndex]<list[maxChildIndex]){
+            Swap(&list[newValueIndex],&list[maxChildIndex]);
+            newValueIndex=maxChildIndex;
+        }
+        else break;
+    }
+}
+
+int* Heapify(int list[],int size){
+    int i,*heap;
+    heap= (int*)malloc(sizeof (int)*size);
+    for (i = 0; i < size; ++i) {
+        heap[i]= list[i];
+        BottomUp(heap,i+1);
+    }
+    return heap;
+}
